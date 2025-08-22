75112e81f9569fd430e3926feae4539710dc6ed8
zzanghun <radioos1009@nate.com>
2025-08-22 21:03:04 +0900

    도커 파일 추가

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, Spring Boot^^!";
+        return "Hello, Spring Boot^^^^!";
     }
 }
