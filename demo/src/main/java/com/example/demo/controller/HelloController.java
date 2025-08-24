e4910c3769de461c032aa3be9c1276b268e37b25
zzanghun <radioos1009@nate.com>
2025-08-24 19:32:40 +0900

    blue

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
 
-        return "Hello, Spring Boot 성공이야 ㅋㅋㅋㅋ 너무 좋다 ^^^^!";
+        return "Hello, Spring Boot 성공이야 ㅋㅋㅋㅋ 너무 좋다 ^^^^! blue";
     }
 
     @GetMapping("/bye")
