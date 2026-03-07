6770ae7d30f5916d2626ff2611d88082752f24fc
zzanghun <radioos1009@nate.com>
2026-03-07 21:29:52 +0900

    v1 추가

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
 
-        return "Hello, Spring Boot 성공이야 ㅋㅋㅋㅋ 너무 좋다 ^^^^! green OK!!!!";
+        return "Hello, Spring Boot 성공이야 ㅋㅋㅋㅋ 너무 좋다 ^^^^! green OK!!!!v1";
     }
 
     @GetMapping("/bye")
